24321de03ab73b5eb3ce0b4bee1b2566c0c61490
Eduardo <eduardo@hipcom.com.br>
2019-01-08 09:02:44 -0200

    Ordenar em java

--- src/Aula12/Ordenar.java
+++ src/Aula12/Ordenar.java
@@ -3,61 +3,83 @@ package Aula12;
 public class Ordenar {
     public static void main(String[] args) {
 
-        long tempoInicial = System.currentTimeMillis();
-
         System.out.println("Ordenar Integer:");
-        Integer vetor[] = {87,21,54,51,49822,6,894,3,9,651,963,1};
-        //Integer vetor[] = {9,6,3,1}; //87,21,54,51,6,894,3,9,651,963,1};
-        bubbleSortInt(vetor);
+        Integer arrayInteiro[] = {87,21,54,51,49822,6,894,3,9,651,963,1};
+        bubbleSortInt(arrayInteiro,true);
+        bubbleSortInt(arrayInteiro,false);
 
         System.out.println("Ordenar String:");
         String vetorTexto[] = {"zz", "aa", "cc","98","01","00", "hh", "bb", "ee", "ll"};
-        bubbleSortString(vetorTexto);
-
-        long tempoFinal = System.currentTimeMillis();
-
-        System.out.println("Executado em = " + (tempoFinal - tempoInicial) + " ms");
+        bubbleSortString(vetorTexto,true);
+        bubbleSortString(vetorTexto,false);
 
     }
 
-    private static void bubbleSortInt(Integer vetor[]) {
+    private static void bubbleSortInt(Integer arrayInteiros[], Boolean orderAZ) {
         boolean troca = true;
         int aux;
+        int quantos;
         while (troca) {
             troca = false;
-            for (int i = 0; i < vetor.length - 1; i++) {
-                if (vetor[i] > vetor[i + 1]) {
-                    aux = vetor[i];
-                    vetor[i] = vetor[i + 1];
-                    vetor[i + 1] = aux;
-                    troca = true;
+            for (int i = 0; i < arrayInteiros.length - 1; i++) {
+                if (orderAZ) {
+                    if (arrayInteiros[i] > arrayInteiros[i + 1]) {
+                        aux = arrayInteiros[i];
+                        arrayInteiros[i] = arrayInteiros[i + 1];
+                        arrayInteiros[i + 1] = aux;
+                        troca = true;
+                    }
+                } else {
+                    if (arrayInteiros[i] < arrayInteiros[i + 1]) {
+                        aux = arrayInteiros[i];
+                        arrayInteiros[i] = arrayInteiros[i + 1];
+                        arrayInteiros[i + 1] = aux;
+                        troca = true;
+                    }
+
                 }
             }
         }
-        for (int i = 0; i < vetor.length; i++) {
-            System.out.println(vetor[i]);
+        quantos=0;
+        aux=0;
+        for (int i = 0; i < arrayInteiros.length; i++) {
+            System.out.println(arrayInteiros[i]);
+            aux= arrayInteiros[i] + aux;
+            quantos++;
         }
+        System.out.println("A soma é: "+aux);
+        System.out.println("Total de itens somados: "+quantos+" e a media é: "+aux/quantos);
     }
 
-    private static void bubbleSortString(String vetor[]) {
+    private static void bubbleSortString(String arrayString[], Boolean orderAZ) {
         boolean troca = true;
         String aux;
         while (troca) {
             troca = false;
-            for (int i = 0; i < vetor.length - 1; i++) {
+            for (int i = 0; i < arrayString.length - 1; i++) {
                 //System.out.println();
-                if (vetor[i].compareTo(vetor[i + 1]) > 0) {
-                    aux = vetor[i];
-                    vetor[i] = vetor[i + 1];
-                    vetor[i + 1] = aux;
-                    troca = true;
+                if (orderAZ) {
+                    if (arrayString[i].compareTo(arrayString[i + 1]) > 0) {
+                        aux = arrayString[i];
+                        arrayString[i] = arrayString[i + 1];
+                        arrayString[i + 1] = aux;
+                        troca = true;
+                    }
+                } else {
+                    if (arrayString[i].compareTo(arrayString[i + 1]) < 0) {
+                        aux = arrayString[i];
+                        arrayString[i] = arrayString[i + 1];
+                        arrayString[i + 1] = aux;
+                        troca = true;
+                    }
                 }
             }
         }
-        for (int i = 0; i < vetor.length; i++) {
-            System.out.println(vetor[i]+",");
+        for (int i = 0; i < arrayString.length; i++) {
+            System.out.println(arrayString[i]+",");
+            if (arrayString[i].toUpperCase().startsWith("A")) {
+                System.out.println(arrayString[i]+" começa com 'A'");
+            }
         }
     }
-
-
 }
